30429047d42ec82fb12dc6e285fb98484fafb8fb
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-08-26 17:15:17 +0600

    add collectionview

--- avito_test/avito_test/Controllers/ViewController.swift
+++ avito_test/avito_test/Controllers/ViewController.swift
@@ -1,30 +1,116 @@
+////
+////  ViewController.swift
+////  avito_test
+////
+////  Created by Анастасия Здобнова on 25.08.2023.
+////
 //
-//  ViewController.swift
-//  avito_test
+//import UIKit
 //
-//  Created by Анастасия Здобнова on 25.08.2023.
+//class ViewController: UIViewController {
+//    let apiManager = APIManager()
+//    var productData: ProductData?
+//    let label = UILabel()
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        view.backgroundColor = .green
+//        label.textAlignment = .center
+//        label.textColor = .white
+//        label.font = UIFont.boldSystemFont(ofSize: 24)
+//        
+//        // Устанавливаем позицию и размер UILabel
+//        label.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 50)
+//        // Добавляем UILabel на экран
+//        view.addSubview(label)
+//        fetchProductData()
+//    }
+//    
+//    func fetchProductData() {
+//        label.text = "loading"
+//        guard let url = URL(string: "https://www.avito.st/s/interns-ios/main-page.json") else {
+//            return
+//        }
+//        
+//        apiManager.fetchData(url: url) { (result: APIResult<ProductData>) in
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success(let productData):
+//                    self.productData = productData
+//                    self.label.text = "загрузилось"
+//                    print(productData)
+//                case .failure(let error):
+//                    self.label.text = ("Error: \(error)")
+//                }
+//            }
+//        }
+//    }
+//}
 //
 
 import UIKit
+
+enum ViewState {
+    case loading
+    case success
+    case error(Error)
+}
 
 class ViewController: UIViewController {
     let apiManager = APIManager()
     var productData: ProductData?
     let label = UILabel()
+    var collectionView: UICollectionView!
     
+    var state: ViewState = .loading {
+        didSet {
+            updateUI()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .green
+        setupUI()
+        fetchProductData()
+    }
+    
+    private func setupUI() {
         label.textAlignment = .center
         label.textColor = .white
         label.font = UIFont.boldSystemFont(ofSize: 24)
-        
-        // Устанавливаем позицию и размер UILabel
         label.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 50)
-        // Добавляем UILabel на экран
         view.addSubview(label)
-        fetchProductData()
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 150, width: view.frame.width, height: view.frame.height - 150), collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+    }
+    
+    private func updateUI() {
+        DispatchQueue.main.async {
+            switch self.state {
+            case .loading:
+                self.label.text = "loading"
+                // Hide or show relevant UI elements for loading state
+                self.collectionView.isHidden = true
+            case .success:
+                self.label.isHidden = true
+                // Update your UI to display collectionView with data
+                self.collectionView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+                self.collectionView.reloadData()
+                self.collectionView.isHidden = false
+            case .error(let error):
+                self.label.text = ("Error: \(error)")
+                // Show error message or handle error state
+                self.collectionView.isHidden = true
+            }
+        }
     }
     
     func fetchProductData() {
@@ -38,13 +124,38 @@ class ViewController: UIViewController {
                 switch result {
                 case .success(let productData):
                     self.productData = productData
-                    self.label.text = "загрузилось"
+                    self.state = .success
                     print(productData)
                 case .failure(let error):
-                    self.label.text = ("Error: \(error)")
+                    self.state = .error(error)
                 }
             }
         }
     }
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return productData?.advertisements.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .gray
+        
+        // Display title in cell
+        if let title = productData?.advertisements[indexPath.item].title {
+            let titleLabel = UILabel(frame: cell.contentView.bounds)
+            titleLabel.text = title
+            titleLabel.textAlignment = .center
+            titleLabel.textColor = .black
+            cell.contentView.addSubview(titleLabel)
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 2 - 10, height: 50)
+    }
+}
---
+++ avito_test/avito_test/View/ProductCell.swift
@@ -0,0 +1,35 @@
+//
+//  ProductCell.swift
+//  avito_test
+//
+//  Created by Анастасия Здобнова on 26.08.2023.
+//
+import Foundation
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
